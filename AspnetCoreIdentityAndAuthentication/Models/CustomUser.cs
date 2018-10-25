1655ffde30e89d8377bdd1ea20d965393d9001e6
Volkan Genç <libertymsdn55@hotmail.com>
2018-10-25 15:59:49 +0300

    Custom User Definition

---
+++ AspnetCoreIdentityAndAuthentication/Models/CustomUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspnetCoreIdentityAndAuthentication.Models
+{
+    public class CustomUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SocialNum { get; set; }
+    }
+}
