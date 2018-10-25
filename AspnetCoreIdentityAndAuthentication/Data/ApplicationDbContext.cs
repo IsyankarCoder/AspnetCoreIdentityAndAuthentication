e4f06c297c18b54b6752b9b12a7b65697d4d40a6
Volkan Genç <libertymsdn55@hotmail.com>
2018-10-25 16:19:52 +0300

    entityframwework core eklendi

---
+++ AspnetCoreIdentityAndAuthentication/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspnetCoreIdentityAndAuthentication.Models;
+
+namespace AspnetCoreIdentityAndAuthentication.Data
+{
+    public class ApplicationDbContext 
+        : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+             
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspnetCoreIdentityAndAuthentication.Models.CustomUser>().ToTable("dbo.User");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
