cab521b48f88a2785e673077746bd3fd9437ce0d
Volkan Genç <libertymsdn55@hotmail.com>
2018-10-26 11:41:24 +0300

    Registr View Model

---
+++ AspnetCoreIdentityAndAuthentication/Models/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspnetCoreIdentityAndAuthentication.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage ="The {0} must be at least {2} and at max{1} characters long ",MinimumLength =6)]
+        [DataType(DataType.Password)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+        
+  
+        [StringLength(100, ErrorMessage = "The password and confirmation password do not match ", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "ConfirmPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
